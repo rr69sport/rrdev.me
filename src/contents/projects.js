9ea8fe22af9026ccb44c40cd6c4f96cb6c535c4c
rr69sport <richardramsua@gmail.com>
2023-04-12 08:03:07 -0300

    First commit

---
+++ src/contents/projects.js
@@ -0,0 +1,81 @@
+export const personalProjects = [
+  {
+    title: 'Heads or Tails',
+    description: 'Se ingresan dos opciones y elige una al azar.',
+    screenshot: 'https://raw.githubusercontent.com/rr69sport/heads-or-tails/main/heads-or-tails.png',
+    built: ['HTML', 'CSS', 'JS'],
+    links: [
+      {
+        text: 'GitHub',
+        url: 'https://github.com/rr69sport/heads-or-tails'
+      },
+      {
+        text: 'Demo',
+        url: 'https://rr69sport.github.io/heads-or-tails/'
+      }
+    ]
+  },
+  {
+    title: 'Link Saver',
+    description: 'Aplicación para guardar links en localstorage.',
+    screenshot: 'https://raw.githubusercontent.com/rr69sport/link-saver/main/link-saver.png',
+    built: ['HTML', 'CSS', 'JS', 'LocalStorage'],
+    links: [
+      {
+        text: 'GitHub',
+        url: 'https://github.com/rr69sport/link-saver'
+      },
+      {
+        text: 'Demo',
+        url: 'https://rr69sport.github.io/link-saver/'
+      }
+    ]
+  },
+  {
+    title: 'Movie Finder',
+    description: 'Buscador de películas con un sencillo login y contraseña en localstorage.',
+    screenshot: 'https://raw.githubusercontent.com/rr69sport/movie-finder/master/movie-finder.png',
+    built: ['HTML', 'CSS', 'JS', 'LocalStorage'],
+    links: [
+      {
+        text: 'GitHub',
+        url: 'https://github.com/rr69sport/movie-finder'
+      },
+      {
+        text: 'Demo',
+        url: 'https://rr69sport.github.io/movie-finder/'
+      }
+    ]
+  }
+]
+
+export const frontendmentorProjects = [
+  {
+    title: 'Social media dashboard',
+    screenshot: 'https://raw.githubusercontent.com/frontendmentor-challenge-repos/social-media-dashboard-with-theme-switcher/master/dashboard.png',
+    links: [
+      {
+        text: 'GitHub',
+        url: 'https://github.com/frontendmentor-challenge-repos/social-media-dashboard-with-theme-switcher'
+      },
+      {
+        text: 'Demo',
+        url: 'https://frontendmentor-challenge-repos.github.io/social-media-dashboard-with-theme-switcher/'
+      }
+    ]
+  },
+  {
+    title: 'FAQ accordion card',
+    screenshot: 'https://raw.githubusercontent.com/frontendmentor-challenge-repos/faq-accordion/main/faq.png',
+    links: [
+      {
+        text: 'GitHub',
+        url: 'https://github.com/frontendmentor-challenge-repos/faq-accordion'
+      },
+      {
+        text: 'Demo',
+        url: 'https://frontendmentor-challenge-repos.github.io/faq-accordion/'
+      }
+    ]
+  }
+]
